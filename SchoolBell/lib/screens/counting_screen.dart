7423b21e4debd6544e3095b2aed579e1710d4199
hyeon_ <onhyeon618@gmail.com>
2022-01-12 01:19:54 +0900

    Set rough layout on main - Counting screen

--- SchoolBell/lib/screens/counting_screen.dart
+++ SchoolBell/lib/screens/counting_screen.dart
@@ -5,6 +5,19 @@ class CountingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green);
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '지금은 쉬는 중♡',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 46),
+          Image.asset('assets/character_play.png')
+        ],
+      ),
+    );
   }
 }
