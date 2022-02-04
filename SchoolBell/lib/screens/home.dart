6a107bc654ee5344d1331fc1ca7e561ea74fced0
hyeon_ <onhyeon618@gmail.com>
2022-02-04 23:42:05 +0900

    Send app to background when press physical back button

--- SchoolBell/lib/screens/home.dart
+++ SchoolBell/lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:move_to_background/move_to_background.dart';
 
 import '../models/models.dart';
 import 'screens.dart';
@@ -68,80 +69,86 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: TabBarView(
-          physics: const NeverScrollableScrollPhysics(),
-          controller: _tabController,
-          children: pages,
+    return WillPopScope(
+      onWillPop: () async {
+        MoveToBackground.moveTaskToBack();
+        return false;
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
+            controller: _tabController,
+            children: pages,
+          ),
         ),
-      ),
-      extendBody: true,
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-                icon: const Icon(Icons.access_time),
-                onPressed: () {
-                  _tabController.animateTo(0);
-                }),
-            const SizedBox(width: 40), // 40 == FAB diameter
-            IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed: () {
-                  _tabController.animateTo(1);
-                }),
-          ],
+        extendBody: true,
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                  icon: const Icon(Icons.access_time),
+                  onPressed: () {
+                    _tabController.animateTo(0);
+                  }),
+              const SizedBox(width: 40), // 40 == FAB diameter
+              IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () {
+                    _tabController.animateTo(1);
+                  }),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Provider.of<ClassManager>(context).isCounting
-            ? Icons.notifications_off_outlined
-            : Icons.notifications),
-        onPressed: () async {
-          if (!classManager.isCounting) {
-            var result = await showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const CustomDialog(
-                    dialogType: CustomDialogType.startClass,
-                    title: '오늘 수업은 몇 교시?',
-                    positive: '시작',
-                    negative: '취소',
-                  );
-                });
-            if (result != null && result > 0) {
-              await flutterLocalNotificationsPlugin.show(
-                Random().nextInt(pow(2, 31) as int),
-                null,
-                '열심히 수업 중~! 오늘도 힘내봐요!',
-                _platformChannelSpecifics,
-              );
-              classManager.startClass(result);
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Provider.of<ClassManager>(context).isCounting
+              ? Icons.notifications_off_outlined
+              : Icons.notifications),
+          onPressed: () async {
+            if (!classManager.isCounting) {
+              var result = await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const CustomDialog(
+                      dialogType: CustomDialogType.startClass,
+                      title: '오늘 수업은 몇 교시?',
+                      positive: '시작',
+                      negative: '취소',
+                    );
+                  });
+              if (result != null && result > 0) {
+                await flutterLocalNotificationsPlugin.show(
+                  Random().nextInt(pow(2, 31) as int),
+                  null,
+                  '열심히 수업 중~! 오늘도 힘내봐요!',
+                  _platformChannelSpecifics,
+                );
+                classManager.startClass(result);
+              }
+            } else {
+              var result = await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return CustomDialog(
+                      dialogType: CustomDialogType.endClass,
+                      title: '오늘 수업을 종료할까요?',
+                      content:
+                          '아직 ${classManager.totalClass - classManager.currentClass + 1}교시 남아있어요!',
+                      positive: '계속하기',
+                      negative: '수업 종료',
+                    );
+                  });
+              if (result == -1) {
+                await flutterLocalNotificationsPlugin.cancelAll();
+                classManager.stopClass();
+              }
             }
-          } else {
-            var result = await showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return CustomDialog(
-                    dialogType: CustomDialogType.endClass,
-                    title: '오늘 수업을 종료할까요?',
-                    content:
-                        '아직 ${classManager.totalClass - classManager.currentClass + 1}교시 남아있어요!',
-                    positive: '계속하기',
-                    negative: '수업 종료',
-                  );
-                });
-            if (result == -1) {
-              await flutterLocalNotificationsPlugin.cancelAll();
-              classManager.stopClass();
-            }
-          }
-        },
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
