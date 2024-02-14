0f0938fdf3dbbe9d71bf95b16d9a68da71bfa5ca
오나현 <onhyeon618@gmail.com>
2024-02-14 22:03:00 +0900

    ♻️ 클래스 탭 UI 구조 개편

--- SchoolBell/lib/presentation/screen/class/screen.dart
+++ SchoolBell/lib/presentation/screen/class/screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:school_bell/domain/class_manager.dart';
+import 'package:school_bell/enum/class_state.dart';
 import 'package:vector_graphics/vector_graphics.dart';
 
 class ClassScreen extends StatelessWidget {
@@ -9,77 +10,31 @@ class ClassScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: 상태 관리 방식 고민
+    final int currentState = context.select<ClassManager, int>((ClassManager cm) => cm.currentState);
+    final int currentClass = context.read<ClassManager>().currentClass;
+
+    final String classStr = currentState == 0 ? '' : currentClass.toString();
+
+    final size = MediaQuery.of(context).size.width - 40;
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          titleText(context),
+          Text(
+            // TODO: currentState 타입을 ClassState 으로 변경
+            '$classStr${ClassState.values[currentState].description}',
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
           const SizedBox(height: 46),
-          mainImage(context),
+          SvgPicture(
+            AssetBytesLoader(ClassState.values[currentState].imagePath),
+            width: size,
+            height: size,
+          ),
         ],
       ),
     );
-  }
-
-  Widget titleText(BuildContext context) {
-    final int currentState = context.select<ClassManager, int>((ClassManager cm) => cm.currentState);
-    final int currentClass = context.read<ClassManager>().currentClass;
-
-    switch (currentState) {
-      case CurrentState.waiting:
-        return Text(
-          '지금은 쉬는 중♡',
-          style: Theme.of(context).textTheme.displayLarge,
-        );
-      case CurrentState.inClass:
-        return Text(
-          '$currentClass교시 수업 중…',
-          style: Theme.of(context).textTheme.displayLarge,
-        );
-      case CurrentState.restTime:
-        return Text(
-          '$currentClass교시 쉬는 시간',
-          style: Theme.of(context).textTheme.displayLarge,
-        );
-      default:
-        return Text(
-          '지금은 쉬는 중♡',
-          style: Theme.of(context).textTheme.displayLarge,
-        );
-    }
-  }
-
-  Widget mainImage(BuildContext context) {
-    final int currentState = context.select<ClassManager, int>((ClassManager cm) => cm.currentState);
-
-    final width = MediaQuery.of(context).size.width - 40;
-    final height = MediaQuery.of(context).size.width - 40;
-
-    switch (currentState) {
-      case CurrentState.waiting:
-        return SvgPicture(
-          const AssetBytesLoader('assets/character/character_play.svg.vec'),
-          width: width,
-          height: height,
-        );
-      case CurrentState.inClass:
-        return SvgPicture(
-          const AssetBytesLoader('assets/character/character_study.svg.vec'),
-          width: width,
-          height: height,
-        );
-      case CurrentState.restTime:
-        return SvgPicture(
-          const AssetBytesLoader('assets/character/character_rest.svg.vec'),
-          width: width,
-          height: height,
-        );
-      default:
-        return SvgPicture(
-          const AssetBytesLoader('assets/character/character_play.svg.vec'),
-          width: width,
-          height: height,
-        );
-    }
   }
 }
