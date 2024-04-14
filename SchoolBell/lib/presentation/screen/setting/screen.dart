0e55b3c3d71907a05968274e47f18365e5038dbc
오나현 <onhyeon618@gmail.com>
2024-04-15 00:03:09 +0900

    ♻️ 설정 탭 UI 구조 변경

--- SchoolBell/lib/presentation/screen/setting/screen.dart
+++ SchoolBell/lib/presentation/screen/setting/screen.dart
@@ -6,8 +6,8 @@ import 'package:school_bell/domain/class_manager.dart';
 import 'package:school_bell/domain/setting_manager.dart';
 import 'package:school_bell/navigation/app_state_manager.dart';
 import 'package:school_bell/presentation/screen/setting/widget/category.dart';
-import 'package:school_bell/presentation/screen/setting/widget/item.dart';
-import 'package:school_bell/presentation/screen/setting/widget/item_version.dart';
+import 'package:school_bell/presentation/screen/setting/widget/setting_item.dart';
+import 'package:school_bell/presentation/screen/setting/widget/app_version_item.dart';
 import 'package:school_bell/presentation/widget/custom_dialog.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -30,222 +30,186 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: 상태 관리 방식 변경
+    final bool isCounting = context.select<ClassManager, bool>((ClassManager cm) => cm.isCounting);
+    final String bellModeValue = context.select<SettingManager, String>((SettingManager sm) => sm.bellModeName);
+    final bool isOnTime = context.select<SettingManager, bool>((SettingManager sm) => sm.isOnTime);
+    final String classLengthValue = context.select<SettingManager, String>((SettingManager sm) => sm.classLengthString);
+    final String restLengthValue = context.select<SettingManager, String>((SettingManager sm) => sm.restLengthString);
+    final String classBellValue =
+        context.select<SettingManager, String>((SettingManager sm) => sm.customClassBell ?? sm.classBellString);
+    final String restBellValue =
+        context.select<SettingManager, String>((SettingManager sm) => sm.customRestBell ?? sm.restBellString);
+
     return SingleChildScrollView(
       child: Column(
         children: [
           const SizedBox(height: 16),
+
+          /// 기본 설정
           const SettingCategory(title: '기본 설정'),
-          buildBellModeItem(context),
-          buildClassLengthItem(context),
-          buildRestLengthItem(context),
+          SettingItem(
+            title: '종소리 모드',
+            attribute: bellModeValue,
+            isDisabled: isCounting,
+            onTap: () async {
+              // TODO: dialog 개편
+              final result = await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const CustomDialog(
+                    dialogType: CustomDialogType.setBellMode,
+                    title: '종소리 모드',
+                    positive: '설정하기',
+                    negative: '취소',
+                  );
+                },
+              );
+              if (result != null && result['returnValue'] > -1) {
+                settingManager.setBellMode(result['returnValue']);
+              }
+            },
+          ),
+          SettingItem(
+            title: '한 교시 길이',
+            attribute: classLengthValue,
+            isDisabled: isOnTime || isCounting,
+            onTap: () async {
+              final result = await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const CustomDialog(
+                    dialogType: CustomDialogType.setTimeLength,
+                    positive: '설정하기',
+                    negative: '취소',
+                    forClass: true,
+                  );
+                },
+              );
+              if (result != null && result['returnValue'] > -1) {
+                settingManager.setClassLength(result['returnValue']);
+              }
+            },
+          ),
+          SettingItem(
+            title: '쉬는 시간 길이',
+            attribute: restLengthValue,
+            isDisabled: isOnTime || isCounting,
+            onTap: () async {
+              final result = await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const CustomDialog(
+                    dialogType: CustomDialogType.setTimeLength,
+                    positive: '설정하기',
+                    negative: '취소',
+                    forClass: false,
+                  );
+                },
+              );
+              if (result != null && result['returnValue'] > -1) {
+                settingManager.setRestLength(result['returnValue']);
+              }
+            },
+          ),
           const SizedBox(height: 8),
+
+          /// 종소리 설정
           const SettingCategory(title: '종소리 설정'),
-          buildClassBellItem(context),
-          buildRestBellItem(context),
+          SettingItem(
+            title: '수업 시작 종',
+            attribute: classBellValue,
+            isDisabled: isCounting,
+            onTap: () async {
+              final result = await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const CustomDialog(
+                    dialogType: CustomDialogType.setBellType,
+                    positive: '설정하기',
+                    negative: '취소',
+                    forClass: true,
+                  );
+                },
+              );
+              if (result != null && result['returnValue'] > -1) {
+                if (result['returnValue'] < 9) {
+                  settingManager.setClassBell(result['returnValue']);
+                  settingManager.setCustomClassBell(null);
+                } else {
+                  settingManager.setClassBell(9);
+                  settingManager.setCustomClassBell(result['extra']);
+                }
+              }
+            },
+          ),
+          SettingItem(
+            title: '수업 종료 종',
+            attribute: restBellValue,
+            isDisabled: isCounting,
+            onTap: () async {
+              final result = await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const CustomDialog(
+                    dialogType: CustomDialogType.setBellType,
+                    positive: '설정하기',
+                    negative: '취소',
+                    forClass: false,
+                  );
+                },
+              );
+              if (result != null && result['returnValue'] > -1) {
+                if (result['returnValue'] < 9) {
+                  settingManager.setRestBell(result['returnValue']);
+                  settingManager.setCustomRestBell(null);
+                } else if (result['returnValue'] == 9) {
+                  settingManager.setRestBell(9);
+                  settingManager.setCustomRestBell(result['extra']);
+                }
+              }
+            },
+          ),
           const SizedBox(height: 8),
-          const SettingCategory(title: '어플리케이션'),
-          buildAppVersionItem(context),
+
+          /// 서비스 정보
+          const SettingCategory(title: '서비스 정보'),
+          AppVersionItem(
+            isUpdateAvailable: appUpdateChecker.isUpdateAvailable,
+            onTap: () async {
+              if (appUpdateChecker.isUpdateAvailable) {
+                var result = await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const CustomDialog(
+                      dialogType: CustomDialogType.normalDialog,
+                      title: '업데이트가 가능합니다',
+                      content: '어플의 새 버전이 출시되었어요.\n지금 바로 업데이트 하러 가시겠어요?',
+                      positive: '스토어 가기',
+                      negative: '나중에',
+                    );
+                  },
+                );
+                if (result != null && result['returnValue'] > -1) {
+                  // TODO
+                  // appUpdateChecker.redirectToStore();
+                }
+              } else {
+                Fluttertoast.showToast(
+                  msg: '현재 최신 버전이에요.',
+                  toastLength: Toast.LENGTH_SHORT,
+                );
+              }
+            },
+          ),
           SettingItem(
             title: '오픈소스 라이선스',
-            attribute: '',
             onTap: () {
               Provider.of<AppStateManager>(context, listen: false).openLicensesPage();
             },
           ),
         ],
       ),
-    );
-  }
-
-  Widget buildBellModeItem(BuildContext context) {
-    final bool isCounting = context.select<ClassManager, bool>((ClassManager cm) => cm.isCounting);
-    final String bellModeValue = context.select<SettingManager, String>((SettingManager sm) => sm.bellModeName);
-
-    return SettingItem(
-      title: '종소리 모드',
-      attribute: bellModeValue,
-      isDisabled: isCounting,
-      onTap: () async {
-        if (!isCounting) {
-          var result = await showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return const CustomDialog(
-                dialogType: CustomDialogType.setBellMode,
-                title: '종소리 모드',
-                positive: '설정하기',
-                negative: '취소',
-              );
-            },
-          );
-          if (result != null && result['returnValue'] > -1) {
-            settingManager.setBellMode(result['returnValue']);
-          }
-        }
-      },
-    );
-  }
-
-  Widget buildClassLengthItem(BuildContext context) {
-    final bool isCounting = context.select<ClassManager, bool>((ClassManager cm) => cm.isCounting);
-    final bool isOnTime = context.select<SettingManager, bool>((SettingManager sm) => sm.isOnTime);
-    final String classLengthValue = context.select<SettingManager, String>((SettingManager sm) => sm.classLengthString);
-
-    return SettingItem(
-      title: '한 교시 길이',
-      attribute: classLengthValue,
-      isDisabled: isOnTime || isCounting,
-      onTap: () async {
-        if (!isOnTime && !isCounting) {
-          var result = await showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return const CustomDialog(
-                dialogType: CustomDialogType.setTimeLength,
-                positive: '설정하기',
-                negative: '취소',
-                forClass: true,
-              );
-            },
-          );
-          if (result != null && result['returnValue'] > -1) {
-            settingManager.setClassLength(result['returnValue']);
-          }
-        }
-      },
-    );
-  }
-
-  Widget buildRestLengthItem(BuildContext context) {
-    final bool isCounting = context.select<ClassManager, bool>((ClassManager cm) => cm.isCounting);
-    final bool isOnTime = context.select<SettingManager, bool>((SettingManager sm) => sm.isOnTime);
-    final String restLengthValue = context.select<SettingManager, String>((SettingManager sm) => sm.restLengthString);
-
-    return SettingItem(
-      title: '쉬는 시간 길이',
-      attribute: restLengthValue,
-      isDisabled: isOnTime || isCounting,
-      onTap: () async {
-        if (!isOnTime && !isCounting) {
-          var result = await showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return const CustomDialog(
-                dialogType: CustomDialogType.setTimeLength,
-                positive: '설정하기',
-                negative: '취소',
-                forClass: false,
-              );
-            },
-          );
-          if (result != null && result['returnValue'] > -1) {
-            settingManager.setRestLength(result['returnValue']);
-          }
-        }
-      },
-    );
-  }
-
-  Widget buildClassBellItem(BuildContext context) {
-    final bool isCounting = context.select<ClassManager, bool>((ClassManager cm) => cm.isCounting);
-    final String classBellValue =
-        context.select<SettingManager, String>((SettingManager sm) => sm.customClassBell ?? sm.classBellString);
-
-    return SettingItem(
-      title: '수업 시작 종',
-      attribute: classBellValue,
-      isDisabled: isCounting,
-      onTap: () async {
-        if (!isCounting) {
-          var result = await showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return const CustomDialog(
-                dialogType: CustomDialogType.setBellType,
-                positive: '설정하기',
-                negative: '취소',
-                forClass: true,
-              );
-            },
-          );
-          if (result != null && result['returnValue'] > -1) {
-            if (result['returnValue'] < 9) {
-              settingManager.setClassBell(result['returnValue']);
-              settingManager.setCustomClassBell(null);
-            } else {
-              settingManager.setClassBell(9);
-              settingManager.setCustomClassBell(result['extra']);
-            }
-          }
-        }
-      },
-    );
-  }
-
-  Widget buildRestBellItem(BuildContext context) {
-    final bool isCounting = context.select<ClassManager, bool>((ClassManager cm) => cm.isCounting);
-    final String restBellValue =
-        context.select<SettingManager, String>((SettingManager sm) => sm.customRestBell ?? sm.restBellString);
-
-    return SettingItem(
-      title: '수업 종료 종',
-      attribute: restBellValue,
-      isDisabled: isCounting,
-      onTap: () async {
-        if (!isCounting) {
-          var result = await showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return const CustomDialog(
-                dialogType: CustomDialogType.setBellType,
-                positive: '설정하기',
-                negative: '취소',
-                forClass: false,
-              );
-            },
-          );
-          if (result != null && result['returnValue'] > -1) {
-            if (result['returnValue'] < 9) {
-              settingManager.setRestBell(result['returnValue']);
-              settingManager.setCustomRestBell(null);
-            } else if (result['returnValue'] == 9) {
-              settingManager.setRestBell(9);
-              settingManager.setCustomRestBell(result['extra']);
-            }
-          }
-        }
-      },
-    );
-  }
-
-  Widget buildAppVersionItem(BuildContext context) {
-    return SettingItemVersion(
-      onTap: () async {
-        if (appUpdateChecker.isUpdateAvailable) {
-          var result = await showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return const CustomDialog(
-                dialogType: CustomDialogType.normalDialog,
-                title: '업데이트가 가능합니다',
-                content: '어플의 새 버전이 출시되었어요.\n지금 바로 업데이트 하러 가시겠어요?',
-                positive: '스토어 가기',
-                negative: '나중에',
-              );
-            },
-          );
-          if (result != null && result['returnValue'] > -1) {
-            // TODO
-            // appUpdateChecker.redirectToStore();
-          }
-        } else {
-          Fluttertoast.showToast(
-            msg: '현재 최신 버전이에요.',
-            toastLength: Toast.LENGTH_SHORT,
-          );
-        }
-      },
-      isUpdateAvailable: appUpdateChecker.isUpdateAvailable,
     );
   }
 }
---
+++ SchoolBell/lib/presentation/screen/setting/widget/app_version_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class AppVersionItem extends StatelessWidget {
+  final VoidCallback onTap;
+  final bool isUpdateAvailable;
+
+  const AppVersionItem({
+    super.key,
+    required this.onTap,
+    required this.isUpdateAvailable,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+        child: Row(
+          children: [
+            Text(
+              '어플리케이션 버전',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const Spacer(),
+            Visibility(
+              visible: isUpdateAvailable,
+              child: Container(
+                width: 5,
+                height: 5,
+                margin: const EdgeInsets.only(right: 4, bottom: 8),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            // TODO(onh): FutureBuilder 말고 다른 방식 없나?
+            FutureBuilder(
+              future: PackageInfo.fromPlatform(),
+              builder: (context, AsyncSnapshot<PackageInfo> snapshot) {
+                String versionName = snapshot.data?.version ?? '';
+                return Text(
+                  versionName,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
